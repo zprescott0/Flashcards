179f9889fa437514e183eabd72a7c6540a394745
zprescott0 <76888154+zprescott0@users.noreply.github.com>
2022-08-18 14:17:03 -0400

    Used selector in Topics.js.

--- src/features/topics/Topics.js
+++ src/features/topics/Topics.js
@@ -2,8 +2,11 @@ import NewTopicForm from "../../components/NewTopicForm";
 import { Link } from "react-router-dom";
 import ROUTES from "../../app/routes";
 
+import { selectTopics } from './topicsSliceData.js';
+import { useSelector } from "react-redux";
+
 export default function Topics() {
-  const topics = {}; // replace this with a call to your selector to select all the topics in state
+  const topics = useSelector(selectTopics); // replace this with a call to your selector to select all the topics in state
 
   return (
     <section className="center">
