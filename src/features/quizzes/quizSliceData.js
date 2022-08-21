9b145c6d0d675c633d4a684ea2d61b0df6608b7e
zprescott0 <76888154+zprescott0@users.noreply.github.com>
2022-08-21 19:09:48 -0400

    Created a thunk that dispatches actions for adding quiz to quizzes and topics stores.

--- src/features/quizzes/quizSliceData.js
+++ src/features/quizzes/quizSliceData.js
@@ -19,3 +19,23 @@ export const selectQuizzes = state => state.quizzes.quizzes;
 
 export const { addQuiz } = quizzesSlice.actions;
 export default quizzesSlice.reducer;
+
+const addQuizToQuizzesAndTopics = (data) => {
+    return async (dispatch) => {
+        const quizData = {
+            id: data.id,
+            name: data.name,
+            topicId: data.topicId,
+            cardIds: data.cardIds
+        };
+
+        dispatch(addQuiz(quizData));
+    
+        const topicData = {
+            quizId: data.id,
+            topicId: data.topicId
+        };
+
+        dispatch(addQuizToTopic(topicData));
+    };
+} 
