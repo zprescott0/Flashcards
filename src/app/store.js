c9c1b25dfab2bbfa947d67876333dda7e4f01066
zprescott0 <76888154+zprescott0@users.noreply.github.com>
2022-08-22 15:15:48 -0400

    Set up cardsReducer in store.

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import topicsReducer from '../features/topics/topicsSliceData';
 import quizReducer from '../features/quizzes/quizSliceData';
+import cardsReducer from '../features/cards/cardSliceData';
 
 
 export default configureStore({
   reducer: {
     topics: topicsReducer,
-    quizzes: quizReducer
+    quizzes: quizReducer,
+    cards: cardsReducer
   },
 });
---
+++ src/features/cards/cardSliceData.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const cardsSlice = createSlice({
+    name: 'cards',
+    initialState: {},
+    reducers: {
+        addCard: (state, action) => {
+            state.cards[action.payload.id] = {
+                ...action.payload
+            };
+        }
+    }
+});
+
+export const { addCard } = cardsSlice.actions;
+export default cardsSlice.reducer;
